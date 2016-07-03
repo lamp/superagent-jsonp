1498de8b9a78d51cd1e55c03613cd38f8708ee5e
Matthew Gradidge <matthew.gradidge@gmail.com>
2016-07-02 21:00:18 -0700

    New build for next version

--- dist/superagent-jsonp.js
+++ dist/superagent-jsonp.js
@@ -1,7 +1,11 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var serialise = function serialise(obj) {
-	if (typeof obj != 'object') return obj;
+	if ((typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) != 'object') return obj;
 	var pairs = [];
 	for (var key in obj) {
 		if (null != obj[key]) {
@@ -27,24 +31,36 @@ var jsonp = function jsonp(requestOrConfig) {
 	}
 };
 
-var callbackWrapper = function callbackWrapper(data) {
+jsonp.callbackWrapper = function (data) {
 	var err = null;
 	var res = {
 		body: data
 	};
+	clearTimeout(this._jsonp.timeout);
 
 	this._jsonp.callback.call(this, err, res);
 };
 
-var end = function end(config) {
+jsonp.errorWrapper = function () {
+	var err = new Error('404 NotFound');
+	this._jsonp.callback.call(this, err, null);
+};
+
+var end = function end() {
+	var config = arguments.length <= 0 || arguments[0] === undefined ? { timeout: 1000 } : arguments[0];
+
 	return function (callback) {
+
+		var timeout = setTimeout(jsonp.errorWrapper.bind(this), config.timeout);
+
 		this._jsonp = {
 			callbackParam: config.callbackParam || 'callback',
 			callbackName: 'superagentCallback' + new Date().valueOf() + parseInt(Math.random() * 1000),
-			callback: callback
+			callback: callback,
+			timeout: timeout
 		};
 
-		window[this._jsonp.callbackName] = callbackWrapper.bind(this);
+		window[this._jsonp.callbackName] = jsonp.callbackWrapper.bind(this);
 
 		var params = _defineProperty({}, this._jsonp.callbackParam, this._jsonp.callbackName);
 
